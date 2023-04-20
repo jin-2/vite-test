2581b01b058ee25303ae00a01d4e7804927657ae
이수진 <sujinlee2@jobkorea.co.kr>
2023-04-20 18:18:39 +0900

    Add UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,164 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  AppstoreOutlined,
+  BarChartOutlined,
+  CloudOutlined,
+  ShopOutlined,
+  TeamOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from '@ant-design/icons';
+import { Layout, Menu, theme, Skeleton, Image, Checkbox, Space, Button } from 'antd';
+import React from 'react';
+import { Typography } from 'antd';
+import { Avatar, List } from 'antd';
+import { CheckCircleTwoTone, HeartTwoTone, SmileTwoTone, StarTwoTone, StarFilled, StarOutlined } from '@ant-design/icons';
 
-function App() {
-  const [count, setCount] = useState(0)
+
+const { Title, Text, Link } = Typography;
+const { Header, Content, Footer, Sider } = Layout;
+const items = Array(50).fill(undefined).map((icon, index) => ({
+  key: String(index + 1),
+  icon: '',
+  label: `nav ${index + 1}`,
+}));
+const addedItems = [
+  {
+    key: 'select',
+    icon: <StarFilled />,
+    label: `선택한 글`,
+  },
+  ...items,
+]
+const data = [
+  {
+    title: '임금체불 기업, 잡코리아 알바몬 서비스 이용 못한다',
+  },
+  {
+    title: 'Ant Design Title 2',
+  },
+  {
+    title: 'Ant Design Title 3',
+  },
+  {
+    title: 'Ant Design Title 4',
+  },
+];
+const App = () => {
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
+  const onChange = () => {
+    console.log('change')
+  }
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
-}
+    <Layout hasSider>
+      <Sider
+        style={{
+          overflow: 'auto',
+          height: '100vh',
+          position: 'fixed',
+          left: 0,
+          top: 0,
+          bottom: 0,
+        }}
+      >
+        <div
+          style={{
+            height: 32,
+            margin: 16,
+          }}
+        >
+          <Title level={1} style={{ color: '#fff', fontSize: '24px' }}>News</Title>
+        </div>
+        <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']} items={addedItems} />
+      </Sider>
+      <Layout
+        className="site-layout"
+        style={{
+          marginLeft: 200,
+          minHeight: '100vh'
+        }}
+      >
+        <Header
+          style={{
+            padding: 0,
+            background: colorBgContainer,
+            display: 'flex',
+            alignItems: 'center'
+          }}
+        >
+          <Title level={2} style={{ marginLeft: '24px', marginBottom: 0 }}>nav 4</Title>
+        </Header>
+        <Content
+          style={{
+            margin: '24px 16px 0',
+            overflow: 'initial',
+            display: 'flex',
+            justifyContent: 'center',
+          }}
+        >
+          <div
+            style={{
+              justifyContent: 'center',
+              width: '800px',
+              padding: 24,
+              // textAlign: 'center',
+              background: colorBgContainer,
+            }}
+          >
+            <List
+              itemLayout="horizontal"
+              dataSource={data}
+              renderItem={(item, index) => (
+                <>
 
-export default App
+                <List.Item
+                  extra={
+                    <Image
+                      preview={false}
+                      width={132}
+                      height={90}
+                      alt="logo"
+                      src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
+                    />
+                  }
+                >
+                  <Skeleton avatar title={false} loading={item.loading} active>
+                    {/*<button>*/}
+                    {/*  <StarFilled style={{ color: 'yellowgreen', fontSize: '24px' }} />*/}
+                    {/*  <StarOutlined style={{ color: 'yellowgreen', fontSize: '24px' }} />*/}
+                    {/*</button>*/}
+                    <Button shape="circle" icon={<StarOutlined style={{ color: 'yellowgreen' }} />} />
+                    <List.Item.Meta
+                      style={{ paddingLeft: '10px', paddingRight: '10px' }}
+                      // avatar={<Avatar shape="square" size={132} src="https://randomuser.me/api/portraits/men/29.jpg" />}
+                      title={<>
+                        <Text style={{ fontSize: '13px', color: '#505050' }}>경향신문</Text>
+                        <Text style={{ fontSize: '13px', color: '#eff2f4' }}> | </Text>
+                        <Text style={{ fontSize: '13px', color: '#808080' }}>1일 전</Text>
+                        <Link target="_blank" href="https://ant.design" style={{ display: 'block', fontWeight: 'bold', fontSize: '16px' }}>{item.title}</Link>
+                      </>}
+                      description={<p style={{ color: "#424242" }}><mark>잡코리아</mark>, 구직자 보호 위해 서비스 이용 전면 제한 잡코리아는 취업 플랫폼 잡코리아와 아르바이트 플랫폼 알바몬에서 임금체불 기업의 서비스 이용을 전면 제한했다고 19일 밝혔다.</p>}
+                    />
+                  </Skeleton>
+                </List.Item>
+                </>
+              )}
+            />
+          </div>
+        </Content>
+        <Footer
+          style={{
+            textAlign: 'center',
+          }}
+        >
+          News ©2023 Created by 애자일개발팀
+        </Footer>
+      </Layout>
+    </Layout>
+  );
+};
+export default App;
